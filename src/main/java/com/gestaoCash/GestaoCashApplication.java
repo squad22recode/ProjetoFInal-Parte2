2651f0752084fb0b4be31271e48bf1ce841f74cd
Maicon Nunes <mnunes315@hotmail.com>
2023-11-10 19:40:32 -0300

    Primeiro commit

---
+++ src/main/java/com/gestaoCash/GestaoCashApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoCash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaoCash/GestaoCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoCash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
