941bbebeef8b150f926e161e5397d0d2d8d8c229
Leandro Costa <lcosta200212@gmail.com>
2023-11-15 04:59:24 -0300

    feat: cria metodos de editar, cadastrar e deletar

--- src/main/java/com/gestaoCash/controllers/CourseController.java
+++ src/main/java/com/gestaoCash/controllers/CourseController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gestaoCash.model.Course;
 import com.gestaoCash.services.CourseService;
 
 @Controller
@@ -24,4 +27,42 @@ public class CourseController {
     return "listCourse";
   }
 
+  @GetMapping("/novo")
+  public String showForm(Model model) {
+    var course = new Course();
+    model.addAttribute("curso", course);
+
+    return "formCourse";
+  }
+
+  @PostMapping("/save")
+  public String saveCourse(@ModelAttribute("curso") Course course) {
+    this.courseService.saveCourse(course);
+
+    return "redirect:/cursos";
+  }
+
+  @GetMapping("/editar/{id}")
+  public String showEditForm(@PathVariable Long id, Model model) {
+    var course = this.courseService.findCourseById(id);
+    model.addAttribute("curso", course);
+
+    return "editCourse";
+  }
+
+  // update
+  @PostMapping("/editar/{id}")
+  public String updateCourse(@ModelAttribute("curso") Course course, @PathVariable Long id) {
+    this.courseService.updateCourseById(id, course);
+
+    return "redirect:/cursos";
+  }
+
+  // delete
+  @GetMapping("/excluir/{id}")
+  public String deleteCourse(@PathVariable Long id) {
+    this.courseService.deleteCourseById(id);
+
+    return "redirect:/cursos";
+  }
 }
